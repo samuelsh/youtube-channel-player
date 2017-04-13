8c8ff2ccfdb92484a27f5fbf24f5d99c62e65f93
samuelsh <samuel.sh79@gmail.com>
2017-04-13 05:17:01 +0300

    Fix: spelling

--- youtube-channel-player.php
+++ youtube-channel-player.php
@@ -14,7 +14,7 @@
  *
  * @wordpress-plugin
  * Plugin Name:       Youtube Channel Player
- * Plugin URI:        https://gtihub.com
+ * Plugin URI:        https://github.com
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
  * Author:            dogen
